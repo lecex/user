b0bf8de0f4ef8ccee6004cb75868701067e58f3c
bigrocs <bigrocs@qq.com>
2020-02-18 21:22:51 +0800

    自动提交 git 代码

--- middleware/hander_test.go
+++ middleware/hander_test.go
@@ -5,12 +5,12 @@ import (
 	"fmt"
 	"testing"
 
-	"github.com/lecex/core/config"
+	PB "github.com/lecex/user/proto/permission"
 	"github.com/micro/go-micro/v2/server"
 )
 
 func TestConfig(t *testing.T) {
-	Pata := []config.Permission{
+	Pata := []PB.Permission{
 		{Service: "Users", Method: "Create", Auth: false, Policy: false, Name: "创建用户", Description: "创建新用户权限。"},
 		{Service: "Users", Method: "Exist", Auth: false, Policy: false, Name: "检测用户", Description: "检测用户是否存在权限。"},
 		{Service: "Users", Method: "Get", Auth: true, Policy: false, Name: "用户查询", Description: "查询用户信息权限。"},
