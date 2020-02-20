8b6d9f0825f95d768de93b0b911ca5aac5060284
bigrocs <bigrocs@qq.com>
2020-02-20 16:23:39 +0800

    自动提交 git 代码

--- main.go
+++ main.go
@@ -6,17 +6,19 @@ import (
 	"github.com/micro/go-micro/v2/util/log"
 
 	// 执行数据迁移
-	_ "github.com/lecex/user/providers/migrations"
+	"github.com/lecex/user/config"
 	"github.com/lecex/user/handler"
+	_ "github.com/lecex/user/providers/migrations"
 )
 
 func main() {
+	var Conf = config.Conf
 	service := micro.NewService(
 		micro.Name(Conf.Service),
 		micro.Version(Conf.Version),
 	)
 	service.Init()
-	 // 注册服务
+	// 注册服务
 	handler.Register(service.Server())
 	// Run the server
 	if err := service.Run(); err != nil {
